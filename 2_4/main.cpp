43f60472ce841b30f7423c0748cbf071d6d916b7
Dmitrii Rogoznyi <k911mipt@gmail.com>
2019-10-18 02:21:15 +0300

    queue solution

--- 2_4/main.cpp
+++ 2_4/main.cpp
@@ -1,23 +1,48 @@
 #include <iostream>
 #include <fstream>
-//#include <cstring>
-//#include <algorithm>
+#include <algorithm>
 
-void naiveSolution(const int &n, int* &arr, const int &windowSize) {
-    //O(n^2)
-    int start = 0;
-    for (int end = windowSize; end < n; end++) {
-        int max = arr[start];
-        for (int i = start + 1; i < end; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-        }
-        std::cout << max << " ";
-        start++;
-    }
-
+inline void incModal(int* &cur, int* &left, int* &right) {
+    if (++cur == right)
+        cur = left;
 }
+
+inline int findMaximum(int* &skippedCur, int* &left, int* &right) {
+    int maximum;
+    if (skippedCur == left) {
+        maximum = *(left + 1);
+    } else {
+        maximum = *left;
+    }
+    for (int* p = left + 1; p < skippedCur; p++)
+        if (maximum < *p)
+            maximum = *p;
+    for (int* p = skippedCur + 1; p < right; p++)
+        if (maximum < *p)
+            maximum = *p;
+    return maximum;
+}
+
+void solveWithQueue(const int &n, int* &arr, const int &windowSize) {
+    int* left = arr;
+    int* right = arr + windowSize;
+    int* next = right;
+    int* last = arr + n;
+    int maximum = std::max(*left, findMaximum(left, left, right));
+    std::cout << maximum << " ";
+    int* cur = left;
+    while (next < last) {
+        if (maximum == *cur) {
+            maximum = findMaximum(cur, left, right);
+        }
+        *cur = *next;
+        maximum = std::max(*cur, maximum);
+        std::cout << maximum << " ";
+        incModal(cur, left, right);
+        next++;
+    }
+}
+
 void solve(int n) {
     int* arr = new int[n];
     for (int i = 0; i < n; i++) {
@@ -25,9 +50,13 @@ void solve(int n) {
     }
     int windowSize;
     std::cin >> windowSize;
-    naiveSolution(n, arr, windowSize);
-
-    std::cout << windowSize << std::endl;
+    if (windowSize == 1) {
+        for (int* p = arr; p < arr + n; p++) {
+            std::cout << *p << " ";
+        }
+    } else {
+        solveWithQueue(n, arr, windowSize);
+    }
     delete arr;
 }
 
