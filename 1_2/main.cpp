cb3f857709a74777a193a1c74cf4c3132bdd1e7f
Dmitrii Rogoznyi <k911mipt@gmail.com>
2019-10-24 02:52:52 +0300

    Add task description

--- 1_2/main.cpp
+++ 1_2/main.cpp
@@ -1,3 +1,32 @@
+﻿/*
+# Задание 1_2. Дек с динамическим буфером
+
+| Ограничение времени | 1 секунда                        |
+|---------------------|----------------------------------|
+| Ограничение памяти  | 5Mb                              |
+| Ввод                | стандартный ввод или input.txt   |
+| Вывод               | стандартный вывод или output.txt |
+
+Реализовать дек с динамическим зацикленным буфером.
+Обрабатывать команды `push *` и `pop *`.
+
+## Формат ввода
+
+В первой строке количество команд *`n`*. *`n ≤ 1000000`*.
+
+Каждая команда задаётся как 2 целых числа: a b.
+
+* a = 1 - push front
+* a = 2 - pop front
+* a = 3 - push back
+* a = 4 - pop back
+
+Если дана команда `pop *`, то число `b` - ожидаемое значение. Если команда `pop *` вызвана для пустой структуры данных, то ожидается `-1`.
+
+Амортизированное время добавления и удаления элемента: O(1)
+Потребляемая память O(n)
+*/
+
 #include <iostream>
 #include <fstream>
 
@@ -124,12 +153,13 @@ namespace made {
 }
 
 int main() {
-    std::ifstream in("input.txt");
-    std::cin.rdbuf(in.rdbuf());
-    int n;
+    std::ifstream input_stream("input.txt");
+    std::cin.rdbuf(input_stream.rdbuf());
+    int n = 0;
     std::cin >> n;
     made::stl::Dequeue<int> dequeue;
-    int operation, value;
+    int operation = 0;
+    int value = 0;
     for (int i = 0; i < n; i++) {
         std::cin >> operation >> value;
         switch (operation) {
