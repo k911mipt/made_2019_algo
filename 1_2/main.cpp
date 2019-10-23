19fe1775bfe9d21d20abbfbdb23e05252163f503
Dmitrii Rogoznyi <k911mipt@gmail.com>
2019-10-24 02:42:08 +0300

    refactoring

--- 1_2/main.cpp
+++ 1_2/main.cpp
@@ -1,118 +1,126 @@
 #include <iostream>
 #include <fstream>
 
-#define std_uint std::size_t
+namespace made {
 
-namespace Dequeue {
+    namespace stl {
 
-    template <typename T>
-    class Dequeue
-    {
-    public:
-        void pushFront(T value);
-        T popFront();
-        void pushBack(T value);
-        T popBack();
-        Dequeue() = default;
-        ~Dequeue() {
-            if (buffer) {
-                delete[] buffer;
+        typedef size_t size_type;
+
+        template <typename T>
+        class Dequeue
+        {
+        public:
+            void PushFront(T value);
+            T PopFront();
+            void PushBack(T value);
+            T PopBack();
+            Dequeue() = default;
+            ~Dequeue() {
+                if (buffer_) {
+                    delete[] buffer_;
+                }
+            }
+        private:
+            const size_type DEFAULT_CAPACITY_ = 1;
+            size_type size_ = 0;
+            size_type capacity_ = DEFAULT_CAPACITY_;
+            T* buffer_ = new T[DEFAULT_CAPACITY_];
+            T* right_ = buffer_ + capacity_;
+            T* head_ = buffer_;
+            T* tail_ = buffer_;
+
+            void ReallocBuffer(size_type newCapacity);
+            void IncSize();
+            void DecSize();
+            void IncModCapacity(T* &cur);
+            void DecModCapacity(T* &cur);
+        };
+
+        template <typename T>
+        void Dequeue<T>::PushFront(T value) {
+            IncSize();
+            *head_ = value;
+            DecModCapacity(head_);
+        }
+
+        template <typename T>
+        T Dequeue<T>::PopFront() {
+            if (size_ == 0) return -1;
+            DecSize();
+            IncModCapacity(head_);
+            return *head_;
+        }
+
+        template <typename T>
+        void Dequeue<T>::PushBack(T value) {
+            IncSize();
+            *tail_ = value;
+            IncModCapacity(tail_);
+        }
+
+        template <typename T>
+        T Dequeue<T>::PopBack() {
+            if (size_ == 0) return -1;
+            DecSize();
+            DecModCapacity(tail_);
+            return *tail_;
+        }
+
+        template<typename T>
+        void Dequeue<T>::ReallocBuffer(size_type newCapacity)
+        {
+            T* newBuffer = new T[newCapacity];
+            IncModCapacity(head_);
+            DecModCapacity(tail_);
+            ++tail_;
+            T* newTail;
+            if (head_ < tail_) {
+                newTail = std::copy(head_, tail_, newBuffer);
+            }
+            else {
+                newTail = std::copy(head_, right_, newBuffer);
+                newTail = std::copy(buffer_, tail_, newTail);
+            }
+            delete[] buffer_;
+            buffer_ = newBuffer;
+            capacity_ = newCapacity;
+            right_ = buffer_ + capacity_;
+            head_ = right_ - 1;
+            tail_ = (newTail == right_) ? buffer_ : newTail;
+        }
+
+        template<typename T>
+        inline void Dequeue<T>::IncSize() {
+            ++size_;
+            if (size_ > capacity_) {
+                ReallocBuffer(capacity_ * 2);
             }
         }
-    private:
-        const std_uint DEFAULT_CAPACITY = 1;
-        std_uint size = 0;
-        std_uint capacity = DEFAULT_CAPACITY;
-        T* buffer = new T[DEFAULT_CAPACITY];
-        T* right = buffer + capacity;
-        T* head = buffer;
-        T* tail = buffer;
 
-        void reallocBuffer(std_uint newCapacity);
-        void incSize();
-        void decSize();
-        void incModal(T* &cur);
-        void decModal(T* &cur);
-    };
-
-    template <typename T>
-    void Dequeue<T>::pushFront(T value) {
-        incSize();
-        *head = value;
-        decModal(head);
-    }
-
-    template <typename T>
-    T Dequeue<T>::popFront() {
-        if (size == 0) return -1;
-        decSize();
-        incModal(head);
-        return *head;
-    }
-
-    template <typename T>
-    void Dequeue<T>::pushBack(T value) {
-        incSize();
-        *tail = value;
-        incModal(tail);
-    }
-
-    template <typename T>
-    T Dequeue<T>::popBack() {
-        if (size == 0) return -1;
-        decSize();
-        decModal(tail);
-        return *tail;
-    }
-
-    template<typename T>
-    void Dequeue<T>::reallocBuffer(std_uint newCapacity)
-    {
-        T* newBuffer = new T[newCapacity];
-        incModal(head);
-        decModal(tail);
-        ++tail;
-        T* newTail;
-        if (head < tail) {
-            newTail = std::copy(head, tail, newBuffer) - 1;
-        } else {
-            newTail = std::copy(head, right, newBuffer);
-            newTail = std::copy(buffer, tail, newTail) - 1;
+        template<typename T>
+        inline void Dequeue<T>::DecSize() {
+            --size_;
+            size_type lowThresholdCapacity = capacity_ / 4;
+            if (size_ < lowThresholdCapacity) {
+                ReallocBuffer(lowThresholdCapacity);
+            }
         }
-        delete[] buffer;
-        buffer = newBuffer;
-        capacity = newCapacity;
-        right = buffer + capacity;
-        head = right - 1;
-        tail = newTail;
-        incModal(tail);
-    }
 
-    template<typename T>
-    inline void Dequeue<T>::incSize() {
-        if (++size > capacity) {
-            reallocBuffer(capacity << 1);
+        template<typename T>
+        inline void Dequeue<T>::IncModCapacity(T* &cur) {
+            if (++cur == right_)
+                cur = buffer_;
         }
-    }
 
-    template<typename T>
-    inline void Dequeue<T>::decSize() {
-        if (--size < capacity >> 2) {
-            reallocBuffer(capacity >> 2);
+        template<typename T>
+        inline void Dequeue<T>::DecModCapacity(T* &cur) {
+            if (--cur < buffer_)
+                cur = right_ - 1;
         }
+
     }
 
-    template<typename T>
-    inline void Dequeue<T>::incModal(T* &cur) {
-        if (++cur == right)
-            cur = buffer;
-    }
-
-    template<typename T>
-    inline void Dequeue<T>::decModal(T* &cur) {
-        if (--cur < buffer)
-            cur = right - 1;
-    }
 }
 
 int main() {
@@ -120,25 +128,25 @@ int main() {
     std::cin.rdbuf(in.rdbuf());
     int n;
     std::cin >> n;
-    Dequeue::Dequeue<int> dequeue;
+    made::stl::Dequeue<int> dequeue;
     int operation, value;
     for (int i = 0; i < n; i++) {
         std::cin >> operation >> value;
         switch (operation) {
         case 1:
-            dequeue.pushFront(value);
+            dequeue.PushFront(value);
             break;
         case 2:
-            if (value != dequeue.popFront()) {
+            if (value != dequeue.PopFront()) {
                 std::cout << "NO" << std::endl;
                 return EXIT_SUCCESS;
             }
             break;
         case 3:
-            dequeue.pushBack(value);
+            dequeue.PushBack(value);
             break;
         case 4:
-            if (value != dequeue.popBack()) {
+            if (value != dequeue.PopBack()) {
                 std::cout << "NO" << std::endl;
                 return EXIT_SUCCESS;
             }
